3ccb54cc38c0c5e9e46aca8a90eccac4b2a340eb
Asbjørn Hegdahl <asbjorn.hegdahl@creuna.no>
2017-11-15 23:05:12 +0100

    prettier

--- src/index.js
+++ src/index.js
@@ -1,92 +1,107 @@
 /* @flow */
-import React, { Component, Children } from "react"
-import raf from "raf"
-import { TransitionMotion, spring } from "react-motion"
+import React, { Component, Children } from "react";
+import raf from "raf";
+import { TransitionMotion, spring } from "react-motion";
 
-import type { Element as ReactElement } from "react"
+import type { Element as ReactElement } from "react";
 
 class FlipMotion extends Component<void, Props, State> {
   state: State;
   children: { [key: any]: HTMLElement };
   constructor(props: Props) {
-    super(props)
+    super(props);
     this.state = {
       shouldMesure: false,
       previousPosition: null,
-      transform: null,
-    }
-    this.children = {}
+      transform: null
+    };
+    this.children = {};
   }
-  getStyles(): { data: ReactElement, style: { [key: any]: any }, key: any} {
-    const { children } = this.props
+  getStyles(): { data: ReactElement, style: { [key: any]: any }, key: any } {
+    const { children } = this.props;
     return Children.map(children, (child, index) => {
       return {
         data: child,
         style: {
           x: spring(0),
           y: spring(0),
-          ...this.state.transform && this.state.transform[child.key] ? this.state.transform[child.key] : null,
+          ...(this.state.transform && this.state.transform[child.key]
+            ? this.state.transform[child.key]
+            : null),
           opacity: spring(1),
-          scale: spring(1),
+          scale: spring(1)
         },
-        key: child.key,
-      }
-    })
+        key: child.key
+      };
+    });
   }
   componentWillReceiveProps(nextProps: Props) {
-    const prevChildren = Children.toArray(this.props.children)
-    const nextChildren = Children.toArray(nextProps.children)
-    if(nextChildren.some((item, index) => !prevChildren[index] || item.key !== prevChildren[index].key)) {
+    const prevChildren = Children.toArray(this.props.children);
+    const nextChildren = Children.toArray(nextProps.children);
+    if (
+      nextChildren.some(
+        (item, index) =>
+          !prevChildren[index] || item.key !== prevChildren[index].key
+      )
+    ) {
       this.setState({
         shouldMesure: true,
         previousPosition: Object.keys(this.children).reduce((acc, key) => {
-          if(this.children[key]) {
-            acc[key] = this.children[key].getBoundingClientRect()
+          if (this.children[key]) {
+            acc[key] = this.children[key].getBoundingClientRect();
           }
-          return acc
+          return acc;
         }, {}),
-        transform: null,
-      })
+        transform: null
+      });
     }
   }
   componentDidUpdate() {
-    if(this.state.shouldMesure) {
+    if (this.state.shouldMesure) {
       raf(() => {
-        this.setState({
-          shouldMesure: false,
-          transform: Object.keys(this.children).reduce((acc, key) => {
-            if(!this.children[key]) {
-              acc[key] = {
-                x: 0,
-                y: 0,
-              }
-              return acc
-            }
-            const nextRect = this.children[key].getBoundingClientRect()
-            if(this.state.previousPosition && this.state.previousPosition[key]) {
-              acc[key] = {
-                x: this.state.previousPosition[key].left - nextRect.left,
-                y: this.state.previousPosition[key].top - nextRect.top,
-              }
-            }
-            return acc
-          }, {}),
-          previousPosition: null,
-        }, () => {
-          if(this.state.transform) {
-            this.setState({
-              transform: Object.keys(this.state.transform).reduce((acc, key) => {
+        this.setState(
+          {
+            shouldMesure: false,
+            transform: Object.keys(this.children).reduce((acc, key) => {
+              if (!this.children[key]) {
                 acc[key] = {
-                  x: spring(0, this.props.springConfig),
-                  y: spring(0, this.props.springConfig),
-                }
-                return acc
-              }, {})
-            })
-            this.children = {}
+                  x: 0,
+                  y: 0
+                };
+                return acc;
+              }
+              const nextRect = this.children[key].getBoundingClientRect();
+              if (
+                this.state.previousPosition &&
+                this.state.previousPosition[key]
+              ) {
+                acc[key] = {
+                  x: this.state.previousPosition[key].left - nextRect.left,
+                  y: this.state.previousPosition[key].top - nextRect.top
+                };
+              }
+              return acc;
+            }, {}),
+            previousPosition: null
+          },
+          () => {
+            if (this.state.transform) {
+              this.setState({
+                transform: Object.keys(
+                  this.state.transform
+                ).reduce((acc, key) => {
+                  acc[key] = {
+                    x: spring(0, this.props.springConfig),
+                    y: spring(0, this.props.springConfig)
+                  };
+                  return acc;
+                }, {})
+              });
+              this.children = {};
+            }
           }
-        })
-      })
+        );
+      });
     }
   }
   willEnter() {
@@ -94,70 +109,68 @@ class FlipMotion extends Component<void, Props, State> {
       x: 0,
       y: 0,
       scale: 0.8,
-      opacity: 0,
-    }
+      opacity: 0
+    };
   }
   render() {
-    const style = this.props.style
-    const childStyle = this.props.childStyle
-    const Component = this.props.component || "div"
-    const ChildComponent = this.props.childComponent || "div"
+    const style = this.props.style;
+    const childStyle = this.props.childStyle;
+    const Component = this.props.component || "div";
+    const ChildComponent = this.props.childComponent || "div";
     return (
-      <TransitionMotion
-        styles={this.getStyles()}
-        willEnter={this.willEnter}
-      >
-        {(styles) =>
-          <Component
-            style={style}
-            className={this.props.className}
-          >
-            {styles.map((item) =>
+      <TransitionMotion styles={this.getStyles()} willEnter={this.willEnter}>
+        {styles => (
+          <Component style={style} className={this.props.className}>
+            {styles.map(item => (
               <ChildComponent
                 key={item.key}
-                style={item.style && {
-                  ...childStyle,
-                  opacity: item.style.opacity,
-                  transform: `translate(${ item.style.x }px, ${ item.style.y }px) scale(${ item.style.scale })`,
-                  WebkitTransform: `translate(${ item.style.x }px, ${ item.style.y }px) scale(${ item.style.scale })`,
-                }}
-                ref={(c) => this.children[item.key] = c}
+                style={
+                  item.style && {
+                    ...childStyle,
+                    opacity: item.style.opacity,
+                    transform: `translate(${item.style.x}px, ${item.style
+                      .y}px) scale(${item.style.scale})`,
+                    WebkitTransform: `translate(${item.style.x}px, ${item.style
+                      .y}px) scale(${item.style.scale})`
+                  }
+                }
+                ref={c => (this.children[item.key] = c)}
               >
                 {item.data}
               </ChildComponent>
-            )}
+            ))}
           </Component>
-        }
+        )}
       </TransitionMotion>
-    )
+    );
   }
 }
 
 type State = {
   shouldMesure: boolean,
   previousPosition: ?{
-    [key: any]: ClientRect,
+    [key: any]: ClientRect
   },
   transform: ?{
     [key: any]: {
       x: number,
-      y: number,
-    },
-  },
-}
+      y: number
+    }
+  }
+};
 
 type Props = {
   springConfig?: {
     stiffness?: number,
     damping?: number,
-    precision?: number,
+    precision?: number
   },
   children?: any,
   style?: Object,
   childStyle?: Object,
   component?: string | ReactClass,
   childComponent?: string | ReactClass,
-  className?: string,
-}
+  className?: string
+};
 
-export default FlipMotion
+export default FlipMotion;
